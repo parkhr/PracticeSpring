d4f24a56b903b60c3141a429d38152cc172ae72c
parkhr <vlhong94@gmail.com>
2022-02-12 01:33:26 +0900

    swagger 적용

--- src/main/java/com/example/demo/domain/employee/EmployeeController.java
+++ src/main/java/com/example/demo/domain/employee/EmployeeController.java
@@ -1,5 +1,8 @@
 package com.example.demo.domain.employee;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +18,13 @@ public class EmployeeController {
 
     private final EmployeeService employeeService;
 
+    @Operation(summary = "test hello", description = "hello api example")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK !!"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    })
     @PostMapping("/employee")
     public void createEmployee(@Valid @RequestBody EmployeeDto.SaveRequest requestDto) {
 
